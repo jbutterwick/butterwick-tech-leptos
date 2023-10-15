5594327c08e45eec87e21e8f588743e66e5eae80
jordan <jordan@butterwick.tech>
2023-10-14 18:06:59 -0600

    add dynamic class, strong result, doublecount derived signal

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use leptos::*;
 
-fn main() {
-    leptos::mount_to_body(|| view! { <App/> })
-}
-
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
+    let double_count = move || count() * 2;
 
     view! {
         <button
@@ -17,8 +14,12 @@ fn App() -> impl IntoView {
             "+"
         </button>
         <p>
-            "Current count: "
-            {count}
+            <strong
+                class:red=move || count() % 2 == 1
+            >
+                "Current count: "
+                {count}
+            </strong>
         </p>
         <button
             on:click=move |_| {
@@ -27,5 +28,37 @@ fn App() -> impl IntoView {
         >
             "-"
         </button>
+
+        // NOTE: self-closing tags like <br> need an explicit /
+        <br/>
+
+        // We'll update this progress bar every time `count` changes
+        <progress
+            // static attributes work as in HTML
+            max="50"
+
+            // passing a function to an attribute
+            // reactively sets that attribute
+            // signals are functions, so this <=> `move || count.get()`
+            value=count
+        >
+        </progress>
+        <br/>
+
+        // This progress bar will use `double_count`
+        // so it should move twice as fast!
+        <progress
+            max="50"
+            // derived signals are functions, so they can also
+            // reactive update the DOM
+            value=double_count
+        >
+        </progress>
+        <p>"Count: " {count}</p>
+        <p>"Double Count: " {double_count}</p>
     }
+}
+
+fn main() {
+    leptos::mount_to_body(|| view! { <App/> })
 }
